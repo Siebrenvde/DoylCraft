f152a71878c5e27dcaea2007a005924d81528842
= <sieb@siebrenvde.dev>
2021-08-29 23:09:52 +0200

    Fix in-game stream announcements

--- src/main/java/dev/siebrenvde/doylcraft/events/DiscordSRVListener.java
+++ src/main/java/dev/siebrenvde/doylcraft/events/DiscordSRVListener.java
@@ -4,16 +4,18 @@ import dev.siebrenvde.doylcraft.utils.Utils;
 import github.scarsz.discordsrv.api.ListenerPriority;
 import github.scarsz.discordsrv.api.Subscribe;
 import github.scarsz.discordsrv.api.events.DiscordGuildMessageReceivedEvent;
+import org.bukkit.Bukkit;
+
 public class DiscordSRVListener {
 
     @Subscribe(priority = ListenerPriority.MONITOR)
     public void discordMessageReceived(DiscordGuildMessageReceivedEvent event) {
 
-        if(event.getChannel().getId().equals("712721773443022900") && event.getAuthor().isBot()) {
+        if(event.getAuthor().getId().equals("856151903736954912")) {
 
             String[] strings = event.getMessage().getContentRaw().split("\n");
 
-            String streamer = strings[0].split(" ")[0];
+            String streamer = strings[0].split(" ")[0].replace("\\", "");
 
             if(streamer.equalsIgnoreCase("doyl_official")) {
                 streamer = "Code";
@@ -22,6 +24,7 @@ public class DiscordSRVListener {
             }
 
             Utils.broadcastHover(streamer, strings[1], strings[2]);
+            Bukkit.getLogger().info(streamer + ", " + strings[1] + ", " + strings[2]);
 
         }
 
