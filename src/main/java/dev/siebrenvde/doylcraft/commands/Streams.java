2ae4c217e76990186dde76c72bdb07f2a04bee00
Siebrenvde <sieb@siebrenvde.dev>
2021-11-13 14:58:46 +0100

    Add basic stream checking

--- src/main/java/dev/siebrenvde/doylcraft/commands/Streams.java
+++ src/main/java/dev/siebrenvde/doylcraft/commands/Streams.java
@@ -2,6 +2,10 @@ package dev.siebrenvde.doylcraft.commands;
 
 import dev.siebrenvde.doylcraft.Main;
 import dev.siebrenvde.doylcraft.utils.Requests;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,7 +32,16 @@ public class Streams implements CommandExecutor {
                 // TODO: Replace with online streamers, using offline for testing
                 ArrayList<String> onlineStreamers = (ArrayList<String>) requests.get("https://api.siebrenvde.dev/doylcraft/streamers").get("offline_streamers");
 
+                TextComponent text = Component.text("Online Streamers:");
+
+                for(String streamer : onlineStreamers) {
+                    text = text.append(Component.text("\n" + streamer).clickEvent(ClickEvent.openUrl("https://twitch.tv/" + streamer)));
+                }
+
+                sender.sendMessage(text);
+
                 // TODO: Send sender message with clickable names leading to Twitch page
+                // TODO: Add stream title and game
                 // TODO: Send message in case no online streamers
 
             }
