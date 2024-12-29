5a63d12d252bdc5befd5853ec5dbdc3d62b4eaeb
Siebrenvde <siebren@siebrenvde.dev>
2024-12-29 22:26:09 +0100

    Improve Discord name checking and profile component

--- src/main/java/dev/siebrenvde/doylcraft/listeners/ChatListener.java
+++ src/main/java/dev/siebrenvde/doylcraft/listeners/ChatListener.java
@@ -3,19 +3,21 @@ package dev.siebrenvde.doylcraft.listeners;
 import dev.siebrenvde.doylcraft.addons.DiscordSRVAddon;
 import dev.siebrenvde.doylcraft.utils.Colours;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.Member;
-import github.scarsz.discordsrv.dependencies.jda.api.entities.Role;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.TextReplacementConfig;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import java.util.regex.Pattern;
+
+import static net.kyori.adventure.text.Component.text;
 
 /**
  * Listener for {@link AsyncChatEvent}
@@ -33,47 +35,50 @@ public class ChatListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPlayerChat(AsyncChatEvent event) {
         Component component = event.originalMessage();
-        String messageContent = ((TextComponent)component).content();
+        String messageContent = PlainTextComponentSerializer.plainText().serialize(component);
 
         // Chat message must contain '@'
         if(!messageContent.contains("@")) { return; }
 
+
         for(Member member : discordSRVAddon.getMembers()) {
             // Message must contain effective name of member
-            if(!messageContent.toLowerCase().contains(member.getEffectiveName().toLowerCase())) { continue; }
+            if(!messageContent.toLowerCase().contains("@" + member.getEffectiveName().toLowerCase())) { continue; }
 
-            TextReplacementConfig config = TextReplacementConfig.builder()
+            component = component.replaceText(TextReplacementConfig.builder()
                 .match(Pattern.compile(Pattern.quote("@" + member.getEffectiveName()), Pattern.CASE_INSENSITIVE))
                 .replacement(profileComponent(member))
-                .build();
-            component = component.replaceText(config);
+                .build()
+            );
         }
 
         event.message(component);
     }
 
     private Component profileComponent(Member member) {
-        Component display = Component.text("@" + member.getEffectiveName(), Colours.DISCORD);
-
-        Component profile = Component.text(member.getEffectiveName(), TextColor.color(member.getColorRaw()));
+        TextComponent.Builder profile = text()
+            .append(text(member.getEffectiveName(), TextColor.color(member.getColorRaw())));
 
         if(!member.getEffectiveName().equals(member.getUser().getAsTag())) {
-            profile = profile.append(Component.newline().append(Component.text(member.getUser().getAsTag(), NamedTextColor.WHITE)));
+            profile.appendNewline();
+            profile.append(text(member.getUser().getAsTag(), NamedTextColor.WHITE));
         }
 
-        int roleAmount = member.getRoles().size();
-        for(int i = 0; i < roleAmount; i++) {
-            if(i == 0) {
-                profile = profile.append(Component.newline());
-            }
-            Role role = member.getRoles().get(i);
-            profile = profile.append(Component.text(role.getName(), TextColor.color(role.getColorRaw())));
-            if(i != (roleAmount - 1)) {
-                profile = profile.append(Component.text(" | ", NamedTextColor.GRAY));
-            }
+        if(!member.getRoles().isEmpty()) {
+            profile.appendNewline();
+            profile.append(Component.join(
+                JoinConfiguration.separator(text(" | ", NamedTextColor.GRAY)),
+                member.getRoles().stream()
+                    .map(role -> text(role.getName(), TextColor.color(role.getColorRaw())))
+                    .toList()
+            ));
         }
 
-        return display.hoverEvent(HoverEvent.showText(profile));
+        return text()
+            .content("@" + member.getEffectiveName())
+            .color(Colours.DISCORD)
+            .hoverEvent(HoverEvent.showText(profile))
+            .build();
     }
 
 }
