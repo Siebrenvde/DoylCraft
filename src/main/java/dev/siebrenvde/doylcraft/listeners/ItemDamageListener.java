edb7ff58190a33765164267e77eb81d24852d817
Siebrenvde <siebren@siebrenvde.dev>
2025-05-03 15:18:21 +0200

    Use data components in ItemDamageListener

--- src/main/java/dev/siebrenvde/doylcraft/listeners/ItemDamageListener.java
+++ src/main/java/dev/siebrenvde/doylcraft/listeners/ItemDamageListener.java
@@ -4,6 +4,7 @@ import dev.siebrenvde.doylcraft.DoylCraft;
 import dev.siebrenvde.doylcraft.player.preferences.PlayerPreferences;
 import dev.siebrenvde.doylcraft.player.PlayerData;
 import dev.siebrenvde.doylcraft.utils.Components;
+import io.papermc.paper.persistence.PersistentDataContainerView;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
@@ -13,21 +14,22 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
-import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.jspecify.annotations.NullMarked;
 
 import java.time.Instant;
-import java.util.Objects;
 
+import static io.papermc.paper.datacomponent.DataComponentTypes.DAMAGE;
+import static io.papermc.paper.datacomponent.DataComponentTypes.MAX_DAMAGE;
+import static java.util.Objects.requireNonNull;
 import static net.kyori.adventure.text.Component.text;
 
 /**
  * Listener for {@link PlayerItemDamageEvent}
  * <p>
- * Warns players when an item reaches a low duration level
+ * Warns players when an item reaches a low durability level
  */
+@SuppressWarnings("UnstableApiUsage")
 @NullMarked
 public class ItemDamageListener implements Listener {
 
@@ -42,37 +44,35 @@ public class ItemDamageListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     private void onItemDamage(PlayerItemDamageEvent event) {
         PlayerPreferences.PrefDurabilityPing prefs = PlayerData.preferences(event.getPlayer()).durabilityPing;
-        if(!prefs.enabled()) return;
+        if (!prefs.enabled()) return;
 
         ItemStack item = event.getItem();
-        Damageable meta = (Damageable) item.getItemMeta();
+        int damage = requireNonNull(item.getData(DAMAGE)) + event.getDamage();
+        int maxDamage = requireNonNull(item.getData(MAX_DAMAGE));
 
-        int damage = meta.getDamage() + event.getDamage();
-        short maxDurability = item.getType().getMaxDurability();
+        int pingDamage = maxDamage - ((maxDamage * prefs.percentage()) / 100);
+        if (damage < pingDamage || damage >= maxDamage) return;
 
-        int pingDurability = maxDurability - ((maxDurability * prefs.percentage()) / 100);
-        if(damage < pingDurability || damage == maxDurability) return;
+        PersistentDataContainerView container = item.getPersistentDataContainer();
 
-        PersistentDataContainer container = meta.getPersistentDataContainer();
-
-        if(!container.has(LAST_PING_KEY, PersistentDataType.LONG)) {
-            pingPlayer(event.getPlayer(), item, damage, maxDurability);
+        if (!container.has(LAST_PING_KEY, PersistentDataType.LONG)) {
+            pingPlayer(event.getPlayer(), item, damage, maxDamage);
             return;
         }
 
-        Instant lastPing = Instant.ofEpochMilli(Objects.requireNonNull(container.get(LAST_PING_KEY, PersistentDataType.LONG)));
-        if(Instant.now().isBefore(lastPing.plusSeconds(prefs.cooldown()))) return;
+        Instant lastPing = Instant.ofEpochMilli(requireNonNull(container.get(LAST_PING_KEY, PersistentDataType.LONG)));
+        if (Instant.now().isBefore(lastPing.plusSeconds(prefs.cooldown()))) return;
 
-        pingPlayer(event.getPlayer(), item, damage, maxDurability);
+        pingPlayer(event.getPlayer(), item, damage, maxDamage);
     }
 
-    private void pingPlayer(Player player, ItemStack item, int damage, short maxDurability) {
+    private void pingPlayer(Player player, ItemStack item, int damage, int maxDamage) {
         item.editPersistentDataContainer(pdc -> pdc.set(LAST_PING_KEY, PersistentDataType.LONG, Instant.now().toEpochMilli()));
         player.sendActionBar(
             text()
                 .append(Components.itemStack(item).colorIfAbsent(NamedTextColor.WHITE))
                 .append(text(" has low durability! "))
-                .append(text((maxDurability - damage) + "/" + maxDurability, NamedTextColor.WHITE))
+                .append(text((maxDamage - damage) + "/" + maxDamage, NamedTextColor.WHITE))
                 .append(text(" remaining"))
                 .colorIfAbsent(NamedTextColor.RED)
         );
