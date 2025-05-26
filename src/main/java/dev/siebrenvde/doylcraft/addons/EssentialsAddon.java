5b3c94477e6726b9eeba421095bd2ceb2aeeb523
Siebrenvde <siebren@siebrenvde.dev>
2025-05-26 02:56:07 +0200

    Remove unused methods from EssentialsAddon

--- src/main/java/dev/siebrenvde/doylcraft/addons/EssentialsAddon.java
+++ src/main/java/dev/siebrenvde/doylcraft/addons/EssentialsAddon.java
@@ -1,19 +1,13 @@
 package dev.siebrenvde.doylcraft.addons;
 
-import com.earth2me.essentials.commands.WarpNotFoundException;
-import com.earth2me.essentials.userstorage.IUserMap;
 import net.ess3.api.IEssentials;
 import net.ess3.api.IUser;
-import net.ess3.api.IWarps;
-import net.ess3.api.InvalidWorldException;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.jspecify.annotations.NullMarked;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
@@ -35,36 +29,6 @@ public class EssentialsAddon {
         CompletableFuture<Boolean> future = new CompletableFuture<>();
         getUser(player).getAsyncTeleport().teleport(location, null, cause, future);
         return future;
-    }
-
-    public static Map<String, Location> getWarps() {
-        IWarps warps = (IWarps) instance.essentials.getWarps();
-        Map<String, Location> result = new HashMap<>();
-        for (String warp : warps.getList()) {
-            try {
-                result.put(warp, warps.getWarp(warp));
-            } catch (WarpNotFoundException | InvalidWorldException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return result;
-    }
-
-    public static Map<IUser, Map<String, Location>> getHomes() {
-        Map<IUser, Map<String, Location>> result = new HashMap<>();
-        IUserMap userMap = instance.essentials.getUsers();
-        userMap.getAllUserUUIDs().stream()
-            .map(userMap::loadUncachedUser)
-            .filter(Objects::nonNull)
-            .forEach(user -> {
-                Map<String, Location> homes = new HashMap<>();
-                user.getHomes().forEach(home -> {
-                    Location location = user.getHome(home);
-                    if (location != null) homes.put(home, location);
-                });
-                if (!homes.isEmpty()) result.put(user, homes);
-            });
-        return result;
     }
 
 }
